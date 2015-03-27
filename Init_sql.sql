d499f666c91d880454e4edcfc41b9a02070b95fa
DarthSerge <capitolin.terry@live.fr>
2015-03-27 15:25:26 +0100

    SQL fini

--- Init_sql.sql
+++ Init_sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 20 Mars 2015 à 17:35
+-- Généré le :  Ven 27 Mars 2015 à 15:17
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -22,8 +22,93 @@ SET time_zone = "+00:00";
 
 DELIMITER $$
 --
+-- Procédures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteLigue`(IN `ligue_id` INT)
+    DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+  DELETE FROM personnel WHERE pers_id IN (
+        SELECT pers_id FROM personnel WHERE lig_id = ligue_id       AND pt_id <> 1
+                          );
+    DELETE FROM ligue WHERE lig_id = ligue_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteUtilisateur`(IN `id` INT)
+    DETERMINISTIC
+BEGIN
+  DELETE FROM personnel WHERE pers_id = id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllLigue`()
+    SQL SECURITY INVOKER
+BEGIN
+  SELECT * FROM ligue;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUtilisateurLigue`(IN `id_Ligue` INT UNSIGNED)
+    SQL SECURITY INVOKER
+    COMMENT 'Renvoi la liste des personnes appartenant à une ligue'
+BEGIN 
+
+SET @id = id_Ligue;
+
+SELECT pers_id,pers_nom,pers_prenom FROM personnel WHERE lig_id = @id;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertLigue`(IN `libelle` VARCHAR(100), IN `description` TEXT)
+    SQL SECURITY INVOKER
+BEGIN
+    
+  INSERT INTO personnel(pers_nom,pers_prenom,pers_email,pt_id,pers_password,lig_id) 
+    VALUES(nom,prenom,mail,statut,epassword,ligue_id);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUtilisateur`(IN `nom` VARCHAR(50), IN `prenom` VARCHAR(50), IN `epassword` VARCHAR(75), IN `ligue_id` INT, IN `statut` INT, IN `mail` VARCHAR(250))
+    SQL SECURITY INVOKER
+BEGIN
+    
+  INSERT INTO personnel(pers_nom,pers_prenom,pers_email,pt_id,pers_password,lig_id) 
+    VALUES(nom,prenom,mail,statut,epassword,ligue_id);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateLigue`(IN `libelle` VARCHAR(100), IN `description` TEXT, IN `ligue_id` INT)
+    SQL SECURITY INVOKER
+BEGIN
+  UPDATE ligue
+    SET lig_libelle = libelle,
+      lig_description = description
+    WHERE 
+      lig_id = ligue_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUtilisateur`(IN `nom` VARCHAR(50), IN `prenom` VARCHAR(50), IN `mail` VARCHAR(250), IN `pass` VARCHAR(75), IN `statut` INT, IN `ligue_id` INT)
+BEGIN
+  UPDATE personnel
+    SET pers_nom = nom,
+      pers_prenom = prenom,
+        pers_email = mail,
+        pt_id = statut,
+        pers_password = pass,
+        lig_id = ligue_id;
+END$$
+
+--
 -- Fonctions
 --
+CREATE DEFINER=`root`@`localhost` FUNCTION `checkAdmin`(`id` INT) RETURNS tinyint(1)
+    SQL SECURITY INVOKER
+BEGIN
+  SET @type = (SELECT pt_id FROM personnel WHERE pers_id = id);
+    
+    CASE
+        WHEN @type = 2 OR @type = 1 THEN RETURN true;
+        WHEN @type = 3 THEN RETURN false;
+    END CASE;
+
+END$$
+
 CREATE DEFINER=`root`@`localhost` FUNCTION `checkId`(`Login` VARCHAR(250), `mdp` VARCHAR(75)) RETURNS int(1) unsigned
     SQL SECURITY INVOKER
 BEGIN 
@@ -48,16 +133,18 @@ DELIMITER ;
 CREATE TABLE IF NOT EXISTS `ligue` (
   `lig_id` int(11) NOT NULL AUTO_INCREMENT,
   `lig_libelle` varchar(100) DEFAULT NULL,
+  `lig_description` text NOT NULL,
   `lig_administrateur_id` int(11) NOT NULL,
   PRIMARY KEY (`lig_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `ligue`
 --
 
-INSERT INTO `ligue` (`lig_id`, `lig_libelle`, `lig_administrateur_id`) VALUES
-(1, 'Ligue Jill Officielle', 0);
+INSERT INTO `ligue` (`lig_id`, `lig_libelle`, `lig_description`, `lig_administrateur_id`) VALUES
+(1, 'Ligue Jill Officielle', 'Ceci est un test', 1),
+(2, 'Ligue ALl Star FAP', 'Ceci est un encore un test', 1);
 
 -- --------------------------------------------------------
 
@@ -76,14 +163,15 @@ CREATE TABLE IF NOT EXISTS `personnel` (
   PRIMARY KEY (`pers_id`),
   KEY `FK_personnel_pt_id` (`pt_id`),
   KEY `FK_personnel_lig_id` (`lig_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `personnel`
 --
 
 INSERT INTO `personnel` (`pers_id`, `pers_nom`, `pers_prenom`, `pers_email`, `pers_password`, `pt_id`, `lig_id`) VALUES
-(1, 'Popolov', 'Serge', 'serge@mail.com', 'serge', 1, 1);
+(1, 'Popolov', 'Serge', 'serge@mail.com', '5a2204e9473c526b4c0993159d652a4e', 1, 1),
+(2, 'pipou', 'poup', 'poupipou@mail.com', 'poupi', 3, 2);
 
 -- --------------------------------------------------------
 
